f8412510f2ebd2c660db753d0f6bd72c6f1ffadd
qbius <my@mail.com>
2022-01-14 17:06:26 +0100

    init

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='discot',  
+    version='0.10',
+    scripts=[] ,
+    author='Michał Surówka',
+    author_email='michalpiotrsurowka@gmail.com',
+    description='Discord bot creation framework',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/qbius/discot',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
